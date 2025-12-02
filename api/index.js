6a06a543d5df74c37d041ffd476afd80a7c0236e
Mohi Uddin Ahmed <mohi.ahmed@innovationsask.ca>
2025-12-02 14:01:43 -0600

    Add dotenv configuration and refactor Vercel serverless function handler

--- api/index.js
+++ api/index.js
@@ -1,4 +1,18 @@
-// Vercel serverless function handler
-import app from "../server/dist/index.js";
+/
 
-export default app;
+dotenv.config();
+
+// Set Vercel environment flag
+process.env.VERCEL = "1";
+
+let handler;
+
+export default async function (req, res) {
+  if (!handler) {
+    // Lazy load the Express app on first request
+    const { default: app } = await import("../server/dist/index.js");
+    handler = app;
+  }
+
+  return handler(req, res);
+}
